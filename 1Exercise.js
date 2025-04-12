76e4781f945cd440d589c82dc26ef743c5136b34
Dani <danielsahagun68@gmail.com>
2025-04-12 14:59:37 +0800

    Add initial JavaScript exercises and HTML file for execution

---
+++ 1Exercise.js
@@ -0,0 +1,10 @@
+//Sum of any three numbers then add 100
+
+let a = 2;
+
+let b = 3;
+
+let c = 4;
+
+let result = a + b + c + 100;
+console.log(result); // 109
---
+++ 2Exercise.js
@@ -0,0 +1,8 @@
+// Same as #1, sum of 2 numbers, then add 200, minus 500
+
+let a = 2;
+
+let b = 3;
+
+let result = a + b + 200 - 500;
+console.log(result); // -295
